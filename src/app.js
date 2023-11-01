e18cbc26e2c4e070979ac2f116236188a963e780
David Lopez <dlopezj@ucentral.edu.co>
2023-11-01 01:34:34 -0500

    Adding cors

--- src/app.js
+++ src/app.js
@@ -10,23 +10,23 @@ import cors from 'cors';
 
 const app = express() // crear un servidor
 
-app.use(cors({
-    origin: 'http://localhost:8080',
-    optionsSuccessStatus: 200
-}))
-
 // app.use(cors({
-//     origin: function(origin, callback){
+//     origin: 'http://localhost:8080',
+//     optionsSuccessStatus: 200
+// }))
+
+app.use(cors({
+    origin: function(origin, callback){
   
-//       if(!origin) return callback(null, true);
-//       if(allowedOrigins.indexOf(origin) === -1){
-//         var msg = 'The CORS policy for this site does not ' +
-//                   'allow access from the specified Origin.';
-//         return callback(new Error(msg), false);
-//       }
-//       return callback(null, true);
-//     }
-//   }))
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  }))
   
 app.use(morgan('dev')); // midelware para intecertar peticiones
 
